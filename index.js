6d720c92caaa777885ca57b8249105d2067812a4
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-19 23:12:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var recipes = new Object()
 
-/*function updateObjectWithKeyAndValue(recipes, key, value) {
+function updateObjectWithKeyAndValue(recipes, key, value) {
   return recipes
 }
-
+/*
 function updateObjectWithKeyAndValue(recipes, key, value) {
   var newRecipes = Object.assign({}, recipes)
   return newRecipes
