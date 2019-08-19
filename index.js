0fc25dcadc07e2a675b705e37e054e661ffe65f4
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-19 23:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value
+  return recipes
+}
